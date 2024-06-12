dba17517dbe9a41c1730d8394f01889d505bbf4c
kerchav <80791975+kerchav@users.noreply.github.com>
2024-06-11 22:58:21 -0300

    1 commit

---
+++ javascript/script.js
@@ -0,0 +1,64 @@
+function exibirMensagem1() {
+    alert("Eu amo o fato de você me amar, até mesmo quando eu mesmo não me amo.");
+}
+
+function exibirMensagem2() {
+    alert("Eu amo seu sorriso.");
+}
+
+function exibirMensagem3() {
+    alert("Eu amo a sua boca e como ela me beija.");
+}
+
+function exibirMensagem4() {
+    alert("Eu amo te olhar dormindo e pensar na sorte que eu tenho por ter você na minha vida.");
+}
+
+function exibirMensagem5() {
+    alert("Eu amo seu jeito. (Mesmo me irritando as vezes)");
+}
+
+function exibirMensagem6() {
+    alert("Eu amo dormir em seus braços.");
+}
+
+function exibirMensagem7() {
+    alert("Eu amo falar de você para todos.");
+}
+
+function exibirMensagem8() {
+    alert("Eu amo pensar nos filhos que nós teremos no futuro.");
+}
+
+function exibirMensagem9() {
+    alert("Eu amo esse dia pq você fez de tudo para que eu me sentisse feliz.");
+}
+
+function exibirMensagem10() {
+    alert("Eu amo a sua risada sincera.");
+}
+
+function exibirMensagem11() {
+    alert("Eu amo tudo o que fazemos em quatro paredes!");
+}
+
+function exibirMensagem12() {
+    alert("Eu amo os nossos dias de preguiça.");
+}
+
+function exibirMensagem13() {
+    alert("Eu amo planejar meu futuro ao seu lado.");
+}
+
+function exibirMensagem14() {
+    alert("Eu amo pensar na nossa casa no futuro.");
+}
+
+function exibirMensagem15() {
+    alert("Eu amo pensar em nossas viagens.");
+}
+
+function exibirMensagem16() {
+    alert("Eu te amo por infinitos motivos, só sou ruim pra me expressar.");
+}
+
